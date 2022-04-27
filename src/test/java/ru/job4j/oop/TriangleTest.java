3b2cd1fc5d11d3da6a88549c99e95aef46f1681f
EkaterinaLom <Lomakinaei@mail.ru>
2022-04-27 12:09:58 +1000

    2.2. Рефакторинг - Площадь треугольника. [#122642]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -20,7 +20,7 @@ public class TriangleTest {
     @Test
     public void when01and01and01ThenMinus1() {
         Point a = new Point(0, 1);
-        Point b = new Point(0, 1);
+        Point b = new Point(0, 0);
         Point c = new Point(0, 1);
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
