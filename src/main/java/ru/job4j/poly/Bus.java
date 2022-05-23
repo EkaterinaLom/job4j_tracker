93edc1a8adff2a769c0586facab50bf4b2fd1700
EkaterinaLom <Lomakinaei@mail.ru>
2022-05-23 13:30:10 +1000

    3. Интерфейс Input [#181092]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,14 +4,18 @@ public class Bus implements Transport {
 
     @Override
     public void trip() {
+        System.out.println("Trip");
     }
 
     @Override
     public void passengers(int passengers) {
+        System.out.println("Trip passengers: " + passengers);
     }
 
     @Override
     public double refill(double fuel) {
-        return 0;
+        int cost = 50;
+        double rsl = cost * fuel;
+        return rsl;
     }
 }
