c30fba80e8e5bffbda56e89dd9589cb509d0b6e6
EkaterinaLom <Lomakinaei@mail.ru>
2022-07-10 19:34:33 +1000

    0. Map, HashMap [#212652 #320303]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("Lomakinaei@mail.ru", "Lomakina EI");
+        map.put("Lomakinaei@mail.ru", "Lomakina Katya");
+        map.put("Ivanov@mail.ru", "Ivanov Ivan");
+
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
