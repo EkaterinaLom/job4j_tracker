a29eecf278f1b8cee79576e4662832985ec9763e
EkaterinaLom <Lomakinaei@mail.ru>
2022-07-23 23:25:30 +1000

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,31 +2,68 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель банковского счета.
+ */
 public class Account {
+    /**
+     * Поле харктеризует реквизиты счета.
+     */
     private String requisite;
+    /**
+     * Поле характеризует баланс счета.
+     */
     private double balance;
 
+    /**
+     * Конструктор банковского счета который будет принимать 2 параметра
+     * и записывать их в наши поля.
+     * @param requisite входящие реквизиты.
+     * @param balance баланс на входе.
+     */
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * Геттер реквизитов, по запросу вернет значение реквизитов.
+     * @return значение реквизитов.
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * Метод - сеттер, при неоходимости предоставит доступ к изменению
+     * реквизитов.
+     * @param requisite принимает значение реквизитов.
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Метод - геттер по запросу вернет значение баланса.
+     * @return возращает значение баланса.
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Сеттер при неоходимости предоставит доступ к изменению баланса.
+     * @param balance принимает значение баланса.
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Переопределение equals, для корректного сравнения объектов.
+     * @param o принимает на входе объект.
+     * @return возвращает результат сравнения (true/false).
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +76,11 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * Переопределение hashCode, для корректного сравнения объектов
+     * коллекции HashMap.
+     * @return возвращает число, которому равен входящий объект.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -2,31 +2,67 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель пользователя банка.
+ */
 public class User {
+    /**
+     * Поле характеризует данные паспорта пользователя.
+     */
     private String passport;
+    /**
+     * поле характеризует имя пользователя.
+     */
     private String username;
 
+    /**
+     * Конструктор описывет пользователя системы который будет принимать 2 параметра
+     * и записывать их в соответствующие поля.
+     * @param passport входящие паспортные данные.
+     * @param username имя пользователя на входе.
+     */
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * Метод - геттер, возвращает значение паспортных данных.
+     * @return возвращает значение паспортных данных
+     */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * Метод - сеттер, при необходимости дает возможность изменить данные паспорта.
+     * @param passport принимает значение паспортных данных.
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * Метод - геттер, возвращает значение имя пользователя.
+     * @return возвращает значение имя пользователя
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Сеттер, обеспечивает доступ к изменению имени пользователя.
+     * @param username на входе принимает значение имени пользователя.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Переопределение equals, для корректного сравнения объектов.
+     * @param o принимает на входе объект
+     * @return возвращает результат сравнения (true/false).
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +75,10 @@ public class User {
         return Objects.equals(passport, user.passport);
     }
 
+    /**
+     * Переопределение hashCode, для корректного сравнения объектов
+     * @return возвращает число, которому равен входящий объект.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(passport);
