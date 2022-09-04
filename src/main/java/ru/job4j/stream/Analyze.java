f5fa90c14f2eb338debc07482362c5e92eb39b4b
EkaterinaLom <Lomakinaei@mail.ru>
2022-09-04 16:38:05 +1000

    2. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,50 @@
+package ru.job4j.stream;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.subjects().stream())
+                .mapToInt(Subject::score)
+                .average()
+                .orElse(0.0);
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream.map(p -> new Tuple(p.name(), p.subjects()
+                .stream()
+                .mapToInt(Subject::score)
+                .average()
+                .orElse(0.0)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.flatMap(p -> p.subjects().stream())
+                .collect(Collectors.groupingBy(Subject::name, LinkedHashMap::new, Collectors.averagingDouble(Subject::score)))
+                .entrySet().stream()
+                .map(s -> new Tuple(s.getKey(), s.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream.flatMap(p -> p.subjects().stream())
+                .collect(Collectors.groupingBy(Subject::name, LinkedHashMap::new, Collectors.summingDouble(Subject::score)))
+                .entrySet().stream()
+                .map(s -> new Tuple(s.getKey(), s.getValue()))
+                .max((s1, s2) -> (int) (s1.score() - s2.score()))
+                .orElse(null);
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(p -> new Tuple(p.name(), p.subjects()
+                        .stream()
+                        .mapToInt(Subject::score)
+                        .sum()))
+                .max((s1, s2) -> (int) (s1.score() - s2.score()))
+                .orElse(null);
+    }
+}
